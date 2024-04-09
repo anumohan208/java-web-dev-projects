87337c1d470679203260711a7662acedcd7b2a69
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2024-04-08 22:34:10 -0500

    Studio Classes and Objects Part 1

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,18 @@
+package org.launchcode;
+
+import java.util.Date;
+
+public class Menu {
+    private ArrayList<MenuItem> menuItems =new ArrayList<MenuItem>();
+    public Date lastUpdated;
+
+    public Menu(ArrayList<MenuItem> menuItems, Date lastUpdated) {
+        this.menuItems = menuItems;
+        this.lastUpdated = lastUpdated;
+    }
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+}
